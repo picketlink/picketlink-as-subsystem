0c0c875cc39c7874e7dfa14966f3a992dd8484f6
pedroigor <pigor.craveiro@gmail.com>
2013-04-12 09:41:30 -0300

    Removing support for publishing the IdentityManager instances for realms in JNDI.

--- src/main/java/org/picketlink/as/subsystem/core/IdentityManagerBeanDefinition.java
+++ src/main/java/org/picketlink/as/subsystem/core/IdentityManagerBeanDefinition.java
@@ -44,6 +44,7 @@ import javax.naming.NamingException;
 import org.jboss.as.naming.deployment.ContextNames;
 import org.picketlink.common.util.StringUtil;
 import org.picketlink.idm.IdentityManager;
+import org.picketlink.idm.IdentityManagerFactory;
 import org.picketlink.idm.internal.DefaultIdentityManager;
 
 /**
@@ -79,16 +80,18 @@ public class IdentityManagerBeanDefinition implements Bean<IdentityManager> {
      */
     @Override
     public IdentityManager create(CreationalContext<IdentityManager> creationalContext) {
-        IdentityManager identityManager = null;
+        IdentityManagerFactory identityManagerFactory = null;
         
         try {
             String formattedJNDIName = this.identityManagerJNDIUrl.replaceAll(ContextNames.JAVA_CONTEXT_SERVICE_NAME.getSimpleName() + ":", "");
             
-            identityManager = (IdentityManager) new InitialContext().lookup(formattedJNDIName);
+            identityManagerFactory = (IdentityManagerFactory) new InitialContext().lookup(formattedJNDIName);
         } catch (NamingException e) {
             throw new RuntimeException("Error looking up IdentityManager from [" + this.identityManagerJNDIUrl + "]", e);
         };
 
+        IdentityManager identityManager = identityManagerFactory.createIdentityManager();
+        
         this.injectionTarget.inject(identityManager, creationalContext);
         this.injectionTarget.postConstruct(identityManager);
 
